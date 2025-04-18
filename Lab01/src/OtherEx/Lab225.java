f5e109019c183835583b93e8aeebb4a4abadbdf1
NMHung2005 <h38810@gmail.com>
2025-04-18 20:50:42 +0700

    re-organized lab01

---
+++ Lab01/src/OtherEx/Lab225.java
@@ -0,0 +1,27 @@
+package OtherEx;
+
+import javax.swing.JOptionPane;
+
+public class Lab225 {
+	public static void main(String[] args) {
+		String strNum1, strNum2;
+
+		strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input the second number",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		double Num1 = Double.parseDouble(strNum1);
+		double Num2 = Double.parseDouble(strNum2);
+		double result = Num1 + Num2;
+		JOptionPane.showMessageDialog(null, result, "Sum is", JOptionPane.INFORMATION_MESSAGE);
+		result = Num1 - Num2;
+		JOptionPane.showMessageDialog(null, result, "Difference is", JOptionPane.INFORMATION_MESSAGE);
+		result = Num1 * Num2;
+		JOptionPane.showMessageDialog(null, result, "Product is", JOptionPane.INFORMATION_MESSAGE);
+		result = Num1 / Num2;
+		JOptionPane.showMessageDialog(null, result, "Quotient is", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, Num1, "Divisor is", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
